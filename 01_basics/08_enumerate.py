c59777ac279a9dddadc9a2d92d76d124ff2c7a6a
tanveeralikhan <tanveer.khanbit@yahoo.com>
2026-09-12 19:37:41 +0530

    new assignments

---
+++ 01_basics/08_enumerate.py
@@ -0,0 +1,6 @@
+skills = ['Python', 'JavaScript', 'C++', 'Java', 'SQL']
+for skill in skills:
+    print(skill)
+
+for index, skill in enumerate(skills):
+    print(f"Index: {index}, Skill: {skill}")
---
+++ 01_basics/09_zip.py
@@ -0,0 +1,5 @@
+names = ['Alice', 'Bob', 'Charlie', 'David']
+ages = [25, 30, 35, 40]
+print("Names and Ages:")
+for name, age in zip(names, ages):
+    print(f"Name: {name}, Age: {age}")
